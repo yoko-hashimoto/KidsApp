1827a2942837e0f0c9dcfe5cec7fd8e3c3214dd0
yoko <otoonorokok.1001@ezweb.ne.jp>
2017-10-13 20:09:51 +0900

    Initial Commit

---
+++ KidsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidsApp
+//
+//  Created by 橋本養子 on 2017/10/13.
+//  Copyright © 2017年 kotokotokoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
